0694e955e827d27771f9abedb9404d0f12458fb2
Bidhu1024 <bidhu1024av@gmail.com>
2023-04-17 20:43:08 +0530

    fix textfield

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+import Main from './components/Main';
 
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+  <>
+<Main />
+  </>
   );
 }
 
---
+++ src/components/Main.jsx
@@ -0,0 +1,81 @@
+import { Box, TextField } from "@mui/material";
+import React,{useState} from "react";
+import icon from "../assets/images/icon";
+const Main = () => {
+
+    const [day,setDay] = useState({
+        value:'',
+        error:false,
+        msg:''
+    })
+    const [month,setMonth] = useState({
+        value:'',
+        error:false,
+        msg:''
+    })
+    const [year,setYear] = useState({
+        value:'',
+        error:false,
+        msg:''
+    })
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+        backgroundColor: "red",
+      }}
+    >
+      <Box sx={{ width: "50%", height: "50%", backgroundColor: "yellow" }}>
+        <Box
+          sx={{
+            display: "flex",
+            width: "60%",
+            justifyContent: "space-between",
+            padding: "20px 40px",
+            borderBottom:1
+          }}
+        >
+          <Box sx={{ width: "30%" }}>
+            <label style={{ fontSize: "1rem" }}>DAY</label>
+            <TextField placeholder="DD"
+            value={day.value}
+            onChange={
+               ()=>{
+                if(day.value === ""){
+                setDay({
+                    value:'',
+                    error:'true',
+                    msg:'This field is required'
+                })
+               }else if(day.value>31){
+                setDay({
+                    value:{day.value},
+                    error:'true',
+                    msg:'This field is required'
+                })
+               }
+               }
+            }
+             />
+          </Box>
+          <Box sx={{ width: "30%" }}>
+            <label>MONTH</label>
+            <TextField placeholder="MM" />
+          </Box>
+          <Box sx={{ width: "30%" }}>
+            <label>YEAR</label>
+            <TextField placeholder="YYYY" />
+          </Box>
+          <img src={icon} alt="" />
+        </Box>
+        <Box></Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Main;
