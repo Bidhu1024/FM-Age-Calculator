4202678b4fd9879372bfe589ee46bea5ebd0c521
Bidhu1024 <bidhu1024av@gmail.com>
2023-04-25 22:03:06 +0530

    err

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,23 +1,38 @@
-import { Box, TextField } from "@mui/material";
-import React,{useState} from "react";
+import { Box, TextField, Typography,Button } from "@mui/material";
+import React, { useState } from "react";
 import icon from "../assets/images/icon";
+import moment from "moment/moment";
 const Main = () => {
+  // const date = new Date()
 
-    const [day,setDay] = useState({
-        value:'',
-        error:false,
-        msg:''
-    })
-    const [month,setMonth] = useState({
-        value:'',
-        error:false,
-        msg:''
-    })
-    const [year,setYear] = useState({
-        value:'',
-        error:false,
-        msg:''
-    })
+  const [day, setDay] = useState({
+    value: "",
+    error: false,
+    msg: "",
+  });
+  const [month, setMonth] = useState({
+    value: "",
+    error: false,
+    msg: "",
+  });
+  const [year, setYear] = useState({
+    value: "",
+    error: false,
+    msg: "",
+  });
+  const days = day.value;
+  const months = month.value;
+  const years = year.value;
+
+  const givenDate = moment({ years, months: months - 1, days });
+  const today = moment();
+
+  const duration = moment.duration(today.diff(givenDate));
+
+  const yearss = duration.years();
+  const monthss = duration.months();
+  const dayss = duration.days();
+
   return (
     <Box
       sx={{
@@ -36,52 +51,95 @@ const Main = () => {
             width: "60%",
             justifyContent: "space-between",
             padding: "20px 40px",
-            borderBottom:1
+            borderBottom: 1,
           }}
         >
           <Box sx={{ width: "30%" }}>
             <label style={{ fontSize: "1rem" }}>DAY</label>
-            <TextField placeholder="DD"
-            value={day.value}
-            helperText={day.msg}
-            onChange={
-               (e)=>{
-
-                let text = e.target.value
-                if(text === ""){
-                setDay({
-                    value:text,
-                    error:'true',
-                    msg:'This field is required'
-                })
-               }else if(text>31){
-                setDay({
-                    value:text,
-                    error:'true',
-                    msg:'Must be a valid day'
-                })
-               }else{
-                setDay({
-                  value:text,
-                  error:false,
-                  msg:''
-                })
-               }
-               }
-            }
-             />
+            <TextField
+              placeholder="DD"
+              value={day.value}
+              helperText={day.msg}
+              onChange={(e) => {
+                let text = e.target.value;
+                if (text === "") {
+                  setDay({
+                    value: text,
+                    error: "true",
+                    msg: "This field is required",
+                  });
+                } else if (text > 31) {
+                  setDay({
+                    value: text,
+                    error: "true",
+                    msg: "Must be a valid day",
+                  });
+                } else {
+                  setDay({
+                    value: text,
+                    error: false,
+                    msg: "",
+                  });
+                }
+              }}
+            />
           </Box>
           <Box sx={{ width: "30%" }}>
             <label>MONTH</label>
-            <TextField placeholder="MM" />
+            <TextField placeholder="MM" onChange={(e)=>{
+                let text = e.target.value;
+                if (text === "") {
+                  setMonth({
+                    value: text,
+                    error: "true",
+                    msg: "This field is required",
+                  });
+                } else if (text > 12) {
+                  setMonth({
+                    value: text,
+                    error: "true",
+                    msg: "Must be a valid month",
+                  });
+                } else {
+                  setMonth({
+                    value: text,
+                    error: false,
+                    msg: "",
+                  });
+            }}} />
           </Box>
           <Box sx={{ width: "30%" }}>
             <label>YEAR</label>
-            <TextField placeholder="YYYY" />
+            <TextField placeholder="YYYY" onChange={(e)=>{
+                let text = e.target.value;
+                if (text === "") {
+                  setYear({
+                    value: text,
+                    error: "true",
+                    msg: "This field is required",
+                  });
+                } else if (text > today) {
+                  setYear({
+                    value: text,
+                    error: "true",
+                    msg: "Must be a valid day",
+                  });
+                } else {
+                  setYear({
+                    value: text,
+                    error: false,
+                    msg: "",
+                  });
+                }
+            }
+            } />
           </Box>
           <img src={icon} alt="" />
         </Box>
-        <Box></Box>
+        <Button></Button>
+        <Box>
+          <Typography>{`${yearss} years, ${monthss} months, and ${dayss} days`}</Typography>
+        </Box>
       </Box>
     </Box>
   );
